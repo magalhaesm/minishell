361950e9394f8a4da9106e78ab83fff1f760f4ed
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-11-12 18:17:35 -0300

    feat(hash table): add a function to get values from table

--- src/table/hash_table_utils.c
+++ src/table/hash_table_utils.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 11:59:43 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/11/12 12:02:49 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/11/12 17:22:36 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,17 @@ void	init_table(t_table *table)
 	table->capacity = 0;
 	table->type_size = sizeof(*table->data);
 	table->data = NULL;
+}
+
+t_bool	table_get(t_table *table, char *key, char **value)
+{
+	t_entry	*entry;
+
+	if (table->count == 0)
+		return (FALSE);
+	entry = find_entry(table->data, table->capacity, key);
+	if (entry->key == NULL)
+		return (FALSE);
+	*value = entry->value;
+	return (TRUE);
 }
