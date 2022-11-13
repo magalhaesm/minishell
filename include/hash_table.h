317b4bd53c4e2f299b55c5dddc11df281ea4daab
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-11-13 19:13:53 -0300

    feat(hash table): add function to free table

--- include/hash_table.h
+++ include/hash_table.h
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:40:01 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/11/13 16:01:08 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/11/13 19:12:07 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ void	init_table(t_table *table);
 t_bool	table_set(t_table *table, char *key, char *value);
 t_bool	table_get(t_table *table, char *key, char **value);
 t_bool	table_delete(t_table *table, char *key);
+void	free_table(t_table *table);
 t_entry	*find_entry(t_entry *entries, int capacity, char *key);
 
 #endif
