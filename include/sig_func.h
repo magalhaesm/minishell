a78c74af5094c36cdcac0870b209974d8ba69719
Ygor Sena <yde-goes@student.42sp.org.br>
2022-11-22 17:47:00 -0300

    refactor(signals): add signals' header and separate it from minishell's header

--- include/sig_func.h
+++ include/sig_func.h
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   sig_func.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ygorgsena <ygorgsena@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/26 14:23:39 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/11/22 17:22:09 by ygorgsena        ###   ########.fr       */
+/*   Created: 2022/11/22 17:21:10 by ygorgsena         #+#    #+#             */
+/*   Updated: 2022/11/22 17:25:42 by ygorgsena        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef SIG_FUNC_H
+# define SIG_FUNC_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+# include <signal.h>
+# include <sys/types.h>
 
 # include "libft.h"
-# include "sig_func.h"
-# include "hash_table.h"
-# include "builtins.h"
 
+typedef void	t_handler(int);
 
+void			wait_user_signals(void);
+void			show_new_prompt(int sig);
 
 #endif
