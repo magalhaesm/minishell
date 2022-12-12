cb80c00663147290b280253449d89940a7da34cb
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-12-12 13:16:54 -0300

    feat(parser): add insert_command

--- src/parser/tree_utils.c
+++ src/parser/tree_utils.c
@@ -6,57 +6,64 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 18:20:05 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/12/08 19:13:00 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/12/12 12:54:39 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tree.h"
 
-static void		swap_nodes(t_node *node, t_node *cmd);
-static t_node	*insert_command(t_node *node, t_node *cmd);
-static t_node	*insert_left(t_node *node, t_node *left);
-static void		append_cmd(t_node *cmd, t_node *param);
+static t_node	*append_cmd(t_node *command, t_node *param);
+static t_node	*insert_left(t_node *parent, t_node *child, int level);
+static t_node	*insert_command(t_node *parent, t_node *child, int level);
 
-t_node	*make_subtree(t_node *node, t_node *left)
+t_node	*subtree(t_node *node, t_node *child)
 {
-	if (node && left)
-		return (insert_left(node, left));
-	else if (node == NULL)
-		return (left);
+	if (node == NULL)
+		return (child);
+	if (node && child)
+		return (insert_left(node, child, 0));
 	return (NULL);
 }
 
-static t_node	*insert_command(t_node *node, t_node *cmd)
+static t_node	*insert_left(t_node *parent, t_node *child, int level)
 {
-	if (node->data.pair.left == NULL)
-		node->data.pair.left = cmd;
+	t_node	**aux;
+
+	aux = &parent->data.pair.left;
+	if (parent->type == COMMAND)
+		return (insert_command(parent, child, level));
+	else if (*aux == NULL)
+		*aux = child;
+	else if ((*aux)->type == COMMAND)
+		*aux = insert_left(child, *aux, level + 1);
 	else
-		append_cmd(node->data.pair.left, cmd);
-	return (node);
+		insert_left(*aux, child, level);
+	return (parent);
 }
 
-static void	append_cmd(t_node *cmd, t_node *param)
+static t_node	*insert_command(t_node *parent, t_node *child, int level)
 {
-	(void) cmd;
-	(void) param;
-	return ;
+	if (child->type == COMMAND)
+	{
+		if (level == 0)
+			return (append_cmd(child, parent));
+		else
+			return (append_cmd(parent, child));
+	}
+	return (insert_left(child, parent, level));
 }
 
-// TODO: please refactor me!
-static t_node	*insert_left(t_node *node, t_node *left)
+static t_node	*append_cmd(t_node *command, t_node *param)
 {
-	if (node->type == COMMAND)
-		return (insert_command(left, node));
-	if (node->data.pair.left == NULL)
-		node->data.pair.left = left;
-	else if (node->data.pair.left->type == COMMAND)
-		swap_nodes(node, left);
-	else
-		insert_left(node->data.pair.left, left);
-	return (node);
-}
+	char	*aux;
 
-static void	swap_nodes(t_node *node, t_node *cmd)
-{
-	node->data.pair.left = insert_left(cmd, node->data.pair.left);
+	aux = command->data.cmd;
+	command->data.cmd = ft_strjoin(aux, " ");
+	free(aux);
+	aux = command->data.cmd;
+	command->data.cmd = ft_strjoin(aux, param->data.cmd);
+	free(param->data.cmd);
+	free(param);
+	free(aux);
+	return (command);
 }
