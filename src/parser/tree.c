769ffb82c33d6a9175d9f86c0d9c316fb3dc15eb
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-12-21 14:35:22 -0300

    fix(parser): allocate enough memory for a command

--- src/parser/tree.c
+++ src/parser/tree.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 15:36:05 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/12/08 18:31:59 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/12/13 19:30:14 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_node	*mkleaf(t_token token)
 
 	node = malloc(sizeof(t_node));
 	node->type = COMMAND;
-	node->data.cmd = malloc(sizeof(t_command) + 1);
+	node->data.cmd = malloc(sizeof(char) * token.length + 1);
 	ft_strlcpy(node->data.cmd, token.start, token.length + 1);
 	return (node);
 }
