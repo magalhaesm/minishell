ae3a018c6b70d549b05407806620b42df88fab1a
Ygor Sena <yde-goes@student.42sp.org.br>
2022-12-22 15:59:58 -0300

    chore: add is_match brief explanation

--- include/expansion.h
+++ include/expansion.h
@@ -6,7 +6,7 @@
 /*   By: yde-goes <yde-goes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 11:04:58 by yde-goes          #+#    #+#             */
-/*   Updated: 2022/12/22 15:11:09 by yde-goes         ###   ########.fr       */
+/*   Updated: 2022/12/22 15:57:11 by yde-goes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,12 @@ typedef struct s_regex
 	t_bool	is_first;
 }	t_regex;
 
+/**
+ * @brief This function compares two strings and returns TURE or FALSE.
+ * The second argument is the pattern to search (i.e. val*ind) and the
+ * first argument is the string to be evaluated (i.e. valgrind). Here,
+ * valgrind is a possible pattern for val*ind. Hence, t_bool is TRUE;
+ */
 t_bool	is_match(char *eval, char *pattern);
 void	init_regex(t_regex *regex, char *eval, char *pattern);
 void	free_dparr(t_bool **split, size_t length);
