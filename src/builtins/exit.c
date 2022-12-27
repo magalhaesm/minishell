e03660f16f65129d58570c7beb4d52a219c8fe21
Ygor Sena <yde-goes@student.42sp.org.br>
2022-12-27 11:55:27 -0300

    chore(exit): removed commented code

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: yde-goes <yde-goes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 17:15:37 by yde-goes          #+#    #+#             */
-/*   Updated: 2022/12/26 19:50:20 by yde-goes         ###   ########.fr       */
+/*   Updated: 2022/12/27 11:54:24 by yde-goes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,50 +31,3 @@ int	ft_exit(char **args)
 {
 	return (EXIT_SUCCESS);
 }
-
-/*
-int	ft_exit(char **args)
-{
-	size_t	i;
-	size_t	j;
-	int		status;
-
-	if (!args[1])
-		exit(1);
-	i = 1;
-	while (args[i])
-	{
-		j = 0;
-		// This can be a static function
-		while (args[i][j] != '\0')
-		{
-			if (!isdigit(args[i][j]))
-			{
-				msh_error("bash: exit: numeric argument required\n", NULL, 0);
-				return (EXIT_FAILURE);
-			}
-			j++;
-		}
-		i++;
-	}
-	if (i > 2)
-	{
-		msh_error("bash: exit: too many arguments\n", NULL, 0);
-		exit(127);
-	}
-	else if (args[1])
-	{
-		status = ft_atoi(args[1]);
-		exit(status);
-	}
-	return (EXIT_SUCCESS);
-}
-*/
-
-/* int	main(void)
-{
-	char *string[] = {"exit", "127", "1456"};
-	bash_exit(string);
-	return (0);
-}
-*/
