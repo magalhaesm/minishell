b4b972bb7f8c461adfd2f5e8c2581c1c41286f06
Ygor Sena <yde-goes@student.42sp.org.br>
2022-12-27 11:52:53 -0300

    fix(test_cd): this test has now a manually created dir cd_folder_test to validade some of cd features

--- tests/test_cd.c
+++ tests/test_cd.c
@@ -6,7 +6,7 @@
 /*   By: yde-goes <yde-goes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 13:25:54 by yde-goes          #+#    #+#             */
-/*   Updated: 2022/12/27 11:24:08 by yde-goes         ###   ########.fr       */
+/*   Updated: 2022/12/27 11:49:03 by yde-goes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,23 +75,16 @@ Test(builtin_cd, cd_false_dir, .init = redirect_all_std, .fini = teardown)
 
 Test(builtin_cd, cd_dir_exist, .init = redirect_all_std, .fini = teardown)
 {
-	char	*input[] = {"cd", "test_folder", NULL};
+	char	*input[] = {"cd", "cd_folder_test", NULL};
 	int		expected;
 	int		result;
-	char	*dir_name = "test_folder";
 	char	*pwd;
 	char	*new_pwd;
 	
 	pwd = ft_getenv("PWD");
 	expected = 0;
-	if (mkdir(dir_name, 777))
-		cr_fail("Couldn't create directory to test " \
-		  		"builtin_cd::cd_dir_exist");
 	result = ft_cd(input);
 	new_pwd = ft_getenv("PWD");
-	if (rmdir(dir_name))
-		cr_fail("Couldn't remove directory used for " \
-		  		"builtin_cd::cd_dir_exist");
 	fflush(stdout);
 	cr_assert(eq(i32, result, expected));
 	cr_assert(ne(str, pwd, new_pwd));
