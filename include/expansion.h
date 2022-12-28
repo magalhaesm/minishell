aa3343a4f803b1191c1df75b0813e9d40ff8e502
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-12-28 17:42:41 -0300

    feat(expander): expand environment variables

--- include/expansion.h
+++ include/expansion.h
@@ -6,7 +6,7 @@
 /*   By: yde-goes <yde-goes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 11:04:58 by yde-goes          #+#    #+#             */
-/*   Updated: 2022/12/22 15:57:11 by yde-goes         ###   ########.fr       */
+/*   Updated: 2022/12/28 17:38:44 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,30 @@ typedef struct s_regex
 }	t_regex;
 
 /**
- * @brief This function compares two strings and returns TURE or FALSE.
+ * @brief This function compares two strings and returns TRUE or FALSE.
  * The second argument is the pattern to search (i.e. val*ind) and the
- * first argument is the string to be evaluated (i.e. valgrind). Here,
- * valgrind is a possible pattern for val*ind. Hence, t_bool is TRUE;
- */
+ * first argument is the string to be evaluated (i.e. valgrind).
+ * Here, valgrind is a possible pattern for val*ind. Hence, t_bool is TRUE. */
 t_bool	is_match(char *eval, char *pattern);
 void	init_regex(t_regex *regex, char *eval, char *pattern);
 void	free_dparr(t_bool **split, size_t length);
+
+/* Expand each string in the table.
+ * Return the table with your modified values. */
+char	**expand(char **cmdtab);
+
+/* Expand each variable found in the string. Whether or not it finds a variable,
+ * it always returns a copy of the original string. */
+char	*var_expansion(char *string);
+
+/* Split a string by quotes. On success return a list of strings,
+ * or null on error. */
+t_list	*split_quotes(char *str);
+
+/* Expand each string into a list. */
+void	expand_quotes(t_list *list);
+
+/* Join each string into a list. Return a complete string. */
+char	*join_quotes(t_list *list);
 
 #endif
---
+++ src/expansion/expand.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 18:17:34 by mdias-ma          #+#    #+#             */
+/*   Updated: 2022/12/28 15:25:15 by mdias-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "helpers.h"
+#include "expansion.h"
+
+static char	*find_key(char *str);
+
+char	**expand(char **cmdtab)
+{
+	int		str;
+	t_list	*list;
+
+	str = 0;
+	while (cmdtab[str])
+	{
+		list = split_quotes(cmdtab[str]);
+		free(cmdtab[str]);
+		expand_quotes(list);
+		cmdtab[str] = join_quotes(list);
+		ft_lstclear(&list, free);
+		str++;
+	}
+	return (cmdtab);
+}
+
+char	*var_expansion(char *string)
+{
+	char	*newstr;
+	char	*key;
+	char	*value;
+
+	string = ft_strdup(string);
+	newstr = string;
+	while (TRUE)
+	{
+		key = find_key(string);
+		if (key)
+		{
+			value = ft_getenv(&key[1]);
+			if (!value)
+				value = "";
+			newstr = str_replace(string, key, value);
+			free(key);
+			free(string);
+			string = newstr;
+		}
+		else
+			break ;
+	}
+	return (newstr);
+}
+
+static char	*find_key(char *str)
+{
+	int	ini;
+	int	pos;
+
+	ini = 0;
+	pos = 0;
+	while (str[pos])
+	{
+		if (str[pos] == '$')
+		{
+			ini = pos++;
+			while (str[pos] && (ft_isalnum(str[pos]) || str[pos] == '_'))
+				pos++;
+			return (ft_substr(str, ini, pos - ini));
+		}
+		else
+			pos++;
+	}
+	return (NULL);
+}
