1718a9a54209783e2f02cc8be30e90324257a922
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-01-04 23:19:08 -0300

    fix(list): accept file redirection

--- src/exec/exec_list.c
+++ src/exec/exec_list.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 22:24:20 by mdias-ma          #+#    #+#             */
-/*   Updated: 2023/01/03 17:58:03 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2023/01/04 22:35:16 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,40 @@
 
 void	exec_and(t_node *node, t_context *ctx)
 {
-	t_node	*lhs;
-	t_node	*rhs;
+	t_node		*lhs;
+	t_node		*rhs;
+	t_context	aux_ctx;
 
 	lhs = node->data.pair.left;
-	rhs = node->data.pair.right;
-	exec_node(lhs, ctx);
+	aux_ctx = *ctx;
+	exec_node(lhs, &aux_ctx);
+	ctx->proc_queue = aux_ctx.proc_queue;
 	reaper(ctx);
 	if (ctx->retcode == EXIT_SUCCESS)
-		exec_node(rhs, ctx);
+	{
+		aux_ctx = *ctx;
+		rhs = node->data.pair.right;
+		exec_node(rhs, &aux_ctx);
+		reaper(&aux_ctx);
+	}
 }
 
 void	exec_or(t_node *node, t_context *ctx)
 {
-	t_node	*lhs;
-	t_node	*rhs;
+	t_node		*lhs;
+	t_node		*rhs;
+	t_context	aux_ctx;
 
 	lhs = node->data.pair.left;
-	rhs = node->data.pair.right;
-	exec_node(lhs, ctx);
+	aux_ctx = *ctx;
+	exec_node(lhs, &aux_ctx);
+	ctx->proc_queue = aux_ctx.proc_queue;
 	reaper(ctx);
 	if (ctx->retcode != EXIT_SUCCESS)
-		exec_node(rhs, ctx);
+	{
+		aux_ctx = *ctx;
+		rhs = node->data.pair.right;
+		exec_node(rhs, &aux_ctx);
+		reaper(&aux_ctx);
+	}
 }
