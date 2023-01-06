15c528046ec36606be3916168aa7504f56f533ce
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-01-06 09:41:39 -0300

    feat(subshell): add parenthesis for priorities

---
+++ src/exec/exec_subshell.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_subshell.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/04 18:03:29 by mdias-ma          #+#    #+#             */
+/*   Updated: 2023/01/05 19:37:08 by mdias-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exec.h"
+
+void	exec_subshell(t_node *node, t_context *ctx)
+{
+	int			pid;
+	int			saved_fd[2];
+	t_node		*list;
+	t_context	aux_ctx;
+
+	list = node->data.pair.left;
+	redirect_io(saved_fd, ctx);
+	aux_ctx = *ctx;
+	pid = pfork();
+	if (pid == FORKED_CHILD)
+	{
+		exec_node(list, &aux_ctx);
+		restore_io(saved_fd);
+		reaper(&aux_ctx);
+		free_pathtab();
+		free_environ();
+		rl_clear_history();
+		free_tree(aux_ctx.to_clean);
+		exit(aux_ctx.retcode);
+	}
+	restore_io(saved_fd);
+	enqueue(pid, ctx);
+	reaper(ctx);
+}
